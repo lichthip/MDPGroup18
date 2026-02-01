a9029134827fee08ad14afef296f76d29f6606e0
Elycts Shaders <YoukuShaders@gmail.com>
2026-02-01 16:38:11 +0800

    Add comments to instruct suggested modifications to downloaded datasets

--- image_rec_train/download_dataset.py
+++ image_rec_train/download_dataset.py
@@ -3,3 +3,11 @@ rf = Roboflow(api_key="YOUR_API_KEY")
 project = rf.workspace("mdp-sho1t").project("mdp-merged")
 version = project.version(1)
 dataset = version.download("yolov8", "datasets/MDP-Merged-1")
+# You may need to update datasets/MDP-Merged-1/data.yaml with the correct paths after downloading:
+# ```
+# # ... other lines ...
+# path: /absolute/path/to/datasets/MDP-Merged-1
+# test: test/images
+# train: train/images
+# val: valid/images
+# # ... other lines ...
